6bf6a5cead036018a743e39983c1908e3ae7d548
catalinstefanoiu <catalin.stefanoiu@student.unitbv.ro>
2024-12-25 21:59:09 +0200

    third commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
+  production: false,
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+    apiKey: "AIzaSyAY1Swai7Ag8g_ha2V85BMzGAenLM-PqHg",
+    authDomain: "friendlychat-61629.firebaseapp.com",
+    projectId: "friendlychat-61629",
+    storageBucket: "friendlychat-61629.firebasestorage.app",
+    messagingSenderId: "774683896625",
+    appId: "1:774683896625:web:fa4c89c414a3999166de10"
+  }
 };
