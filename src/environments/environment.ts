fa862ab5a44971b5e6d528ab1ffd6c0de74dc2c8
catalinstefanoiu <catalin.stefanoiu@student.unitbv.ro>
2024-12-25 22:58:50 +0200

    five commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyAY1Swai7Ag8g_ha2V85BMzGAenLM-PqHg',
+    authDomain: 'friendlychat-61629.firebaseapp.com',
+    databaseURL: 'https://friendlychat-61629.firebaseio.com', // opțional dacă este în configurația ta Firebase
+    projectId: 'friendlychat-61629',
+    storageBucket: 'friendlychat-61629.firebasestorage.app',
+    messagingSenderId: '774683896625',
+    appId: '1:774683896625:web:fa4c89c414a3999166de10',
+    measurementId: 'G-MEASUREMENT_ID', // poate rămâne necompletat dacă nu ai activat Google Analytics
   },
 };
